a714669a86228b895b5575c608c25b3b8ff5e8c8
Jonathan Jeremiah <110501575+JerryOJJ@users.noreply.github.com>
2023-06-07 13:37:16 +0100

    New updates
    Almost done with the SignIn Page

---
+++ app.js
@@ -0,0 +1,8 @@
+let SignupBtn = document.getElementById("SignupBtn");
+let SigninBtn = document.getElementById("SigninBtn");
+let nameField = document.getElementById("nameField");
+let title = document.getElementById("title");
+
+SigninBtn.onclick = Function () {
+    
+}
